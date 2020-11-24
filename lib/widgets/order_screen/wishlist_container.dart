c42d308bb427eab5803b108acc2622eec601c361
shantanu341 <shantanu@livingdesire.in>
2020-11-24 23:37:09 +0530

    wishlist item count added

--- lib/widgets/order_screen/wishlist_container.dart
+++ lib/widgets/order_screen/wishlist_container.dart
@@ -1,8 +1,22 @@
 import 'package:flutter/material.dart';
+import '../../data/data.dart';
+import '../../models/models.dart';
+import '../../widgets/widgets.dart';
 
 class WishlistContainer extends StatelessWidget {
+  final List<Product> wishlistProducts = product;
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: GridView.builder(
+        itemCount: wishlistProducts.length,
+        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 5,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          return ProductItem(product: wishlistProducts[index]);
+        },
+      ),
+    );
   }
 }
