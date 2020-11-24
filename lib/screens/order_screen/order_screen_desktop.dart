85be0261a9eb3c42b42c0a13b73b7d4dd97ce8fc
shantanu341 <shantanu@livingdesire.in>
2020-11-24 20:13:17 +0530

    Order Screen Initialised

---
+++ lib/screens/order_screen/order_screen_desktop.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class OrderScreenDesktop extends StatefulWidget {
+  final TrackingScrollController scrollController;
+
+  const OrderScreenDesktop({
+    Key key,
+    this.scrollController,
+  }) : super(key: key);
+
+  @override
+  _OrderScreenDesktopState createState() => _OrderScreenDesktopState();
+}
+
+class _OrderScreenDesktopState extends State<OrderScreenDesktop> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/screens.dart
+++ lib/screens/screens.dart
@@ -2,3 +2,6 @@ export 'home_screen/home_screen.dart';
 export 'error_screen/error_screen.dart';
 export 'home_screen/home_screen_mobile.dart';
 export 'home_screen/home_screen_desktop.dart';
+export 'order_screen/order_screen.dart';
+export 'order_screen/order_screen_desktop.dart';
+export 'order_screen/order_screen_mobile.dart';
