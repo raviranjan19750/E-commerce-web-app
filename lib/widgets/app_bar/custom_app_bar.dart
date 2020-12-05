652e618fb59fad68446895a890e463fb78c4b27a
shantanu341 <shantanu@livingdesire.in>
2020-12-06 02:12:54 +0530

    button list widget build

--- lib/widgets/app_bar/custom_app_bar.dart
+++ lib/widgets/app_bar/custom_app_bar.dart
@@ -4,30 +4,36 @@ import 'package:living_desire/widgets/app_bar/middle_app_bar.dart';
 import '../widgets.dart';
 import 'package:flutter/material.dart';
 
-
-
 class CustomAppBar extends StatefulWidget with PreferredSizeWidget {
-  
   double size = 160;
   bool visibleMiddleAppBar = true;
   bool visibleSubAppBar = true;
-  
-  CustomAppBar({this.size = 160, this.visibleMiddleAppBar = true,this.visibleSubAppBar = true});
-  
+
+  CustomAppBar(
+      {this.size = 160,
+      this.visibleMiddleAppBar = true,
+      this.visibleSubAppBar = true});
+
   // AppBar has a preferred Size, need to extend Preferred Size widget
-  Size get preferredSize =>  Size.fromHeight(size);
+  Size get preferredSize => Size.fromHeight(size);
   @override
-  _CustomAppBarState createState() => _CustomAppBarState(size: size,visibleMiddleAppBar: visibleMiddleAppBar,visibleSubAppBar: visibleSubAppBar);
+  _CustomAppBarState createState() => _CustomAppBarState(
+      size: size,
+      visibleMiddleAppBar: visibleMiddleAppBar,
+      visibleSubAppBar: visibleSubAppBar);
 }
 
 class _CustomAppBarState extends State<CustomAppBar> {
-
   double size;
   bool visibleMiddleAppBar;
   bool visibleSubAppBar;
 
-  _CustomAppBarState({this.size,this.visibleMiddleAppBar,this.visibleSubAppBar});
-  
+  _CustomAppBarState({
+    this.size,
+    this.visibleMiddleAppBar,
+    this.visibleSubAppBar,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Stack(
@@ -36,8 +42,7 @@ class _CustomAppBarState extends State<CustomAppBar> {
         Column(
           children: [
             Container(
-              padding:  EdgeInsets.only(left: 16,right: 16,top: 8,bottom: 8),
-
+              padding: EdgeInsets.only(left: 16, right: 16, top: 8, bottom: 8),
               width: double.infinity,
               decoration: BoxDecoration(
                 color: Colors.white,
@@ -50,11 +55,8 @@ class _CustomAppBarState extends State<CustomAppBar> {
                 ],
               ),
               child: Row(
-
                 crossAxisAlignment: CrossAxisAlignment.center,
-
                 children: [
-
                   //Logo of the website
                   AppBarLogo(),
                   // WebSite Name
@@ -66,20 +68,19 @@ class _CustomAppBarState extends State<CustomAppBar> {
 
                   AppBarWishlist(),
                   // User Card Login/Signup or Profile
-                  UserCard(isLoggedIn: false,
+                  UserCard(
+                    isLoggedIn: false,
                   ),
                 ],
               ),
             ),
-
-            Visibility(visible : visibleMiddleAppBar ,child: MiddleAppBar())
+            Visibility(visible: visibleMiddleAppBar, child: MiddleAppBar())
           ],
         ),
         Positioned(
-          right: 0,
-          bottom: -32,
-          child: Visibility(visible:visibleSubAppBar,child: SubAppBar())
-        ),
+            right: 0,
+            bottom: -32,
+            child: Visibility(visible: visibleSubAppBar, child: SubAppBar())),
       ],
     );
   }
