f4dba014e669b627462f43aa8f11caa948aa6968
Bhupendra <68356475+itsbhupendra@users.noreply.github.com>
2021-01-13 18:11:35 +0530

    copy to clipboard

--- lib/screens/manage_addresses_screen/my_profile_screen.dart
+++ lib/screens/manage_addresses_screen/my_profile_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:living_desire/ProviderModels/MyProfileProvider.dart';
 import 'package:living_desire/config/configs.dart';
 import 'package:provider/provider.dart';
@@ -218,7 +219,11 @@ class MyProfileScreen extends StatelessWidget{
                           child: IconButton(
 
                             icon: Icon(Icons.copy_outlined),
-                            onPressed: (){},
+                            onPressed: (){
+
+                              Clipboard.setData(new ClipboardData(text: value.referralCode));
+
+                            },
 
                           ),
                         )
