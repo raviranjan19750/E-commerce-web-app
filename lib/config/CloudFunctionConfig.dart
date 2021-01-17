3ee241cd7b66e6c90b6a52ab95f75ddf25ff1aa5
Pratyush Harsh <pratyushharsh2015@gmail.com>
2021-01-18 00:49:06 +0530

    Calling Javascript function from the dart

--- lib/config/CloudFunctionConfig.dart
+++ lib/config/CloudFunctionConfig.dart
@@ -1,13 +1,22 @@
+@JS()
+library living_desire;
+
+import 'package:js/js.dart';
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 import 'package:logger/logger.dart';
 import 'function_config.dart';
-import 'dart:js' as js;
+import 'dart:js_util';
+
 
 var LOG = Logger();
 
+@JS()
+external secret_key();
+
 class CloudFunctionConfig {
+
   static var res = "37b947579b10b34b066d1b01eb2636da1cba6f25";
   static Map<String, String> headers = {
     HttpHeaders.contentTypeHeader: "application/json", // or whatever
@@ -16,6 +25,10 @@ class CloudFunctionConfig {
   };
 
   static Future<http.Response> post(String endPoint, var data) async {
+    // @TODO USe this token according to the need.
+    var promise = secret_key();
+    var res = await promiseToFuture(promise);
+    LOG.i(res);
     return await http.post(FunctionConfig.host + endPoint,
         body: jsonEncode(data), headers: headers);
   }
@@ -26,6 +39,8 @@ class CloudFunctionConfig {
   }
 
   static Future<http.Response> get(String endPoint) async {
+    // var temp = await js.context.callMethod('secret_key');
+    // LOG.i(temp);
     return await http.get(FunctionConfig.host + endPoint, headers: headers);
   }
 
--- web/remote_config.js
+++ web/remote_config.js
@@ -1,18 +1,24 @@
 
 // Get Function Secret key Firebase remote config
 
+
 async function secret_key() {
     const remoteConfig = firebase.remoteConfig();
-
     remoteConfig.settings.minimumFetchIntervalMillis = 3600000;
+    try {
+     await remoteConfig.fetchAndActivate();
+     var res = await remoteConfig.getValue("function_secret_key");
+     return res._value;
+    } catch (e) {
+        console.error(e)
+    }
 
-
-    return await remoteConfig.fetchAndActivate()
-        .then(() => {
-            const val = remoteConfig.getValue("function_secret_key");
-            return val._value;
-        })
-        .catch((err) => {
-            console.error({ err })
-        });
+//    return remoteConfig.fetchAndActivate()
+//        .then(() => {
+//            const val = remoteConfig.getValue("function_secret_key");
+//            return val._value;
+//        })
+//        .catch((err) => {
+//            console.error({ err })
+//        });
 }
