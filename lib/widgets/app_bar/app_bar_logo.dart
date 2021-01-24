eee78d621f1aa5c42a4cbc41d651884913917096
rai02 <devashishrai2000@gmail.com>
2021-01-24 07:14:12 +0530

    WEB-11:Header-Living Desire Click - Go to Homepage

--- lib/widgets/app_bar/app_bar_logo.dart
+++ lib/widgets/app_bar/app_bar_logo.dart
@@ -1,13 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:living_desire/service/navigation_service.dart';
+
+import '../../main.dart';
+import '../../routes.dart';
 
 class AppBarLogo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Image(
-        image: AssetImage('assets/images/logo.jpeg'),
-        width: 48,
-        height: 48,
+      child: InkWell(
+        onTap: () {
+          locator<NavigationService>()
+              .navigateTo(RoutesConfiguration.HOME_PAGE);
+        },
+        child: Image(
+          image: AssetImage('assets/images/logo.jpeg'),
+          width: 48,
+          height: 48,
+        ),
       ),
     );
   }
